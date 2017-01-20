6b18162b0b7efbd262a89b73431f95f38fc8096c
Thibault BERTHIER <tberthie@e2r6p16.42.fr>
2017-01-20 15:59:06 +0100

    up

--- srcs/render.c
+++ srcs/render.c
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/17 12:51:48 by tberthie          #+#    #+#             */
-/*   Updated: 2017/01/20 09:36:31 by tberthie         ###   ########.fr       */
+/*   Updated: 2017/01/20 15:57:45 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,79 @@
 #include <math.h>
 #include <mlx.h>
 
-static void		walls(int x, t_wolf *wolf)
+static double			dist_y(t_wolf *wolf, double alpha)
 {
-	double	rx;
-	double	ry;
-	double	alpha;
+	double	x;
+	double	y;
+	int		i;
 
-	alpha = -FOV / 2 + FOV * x / WINX + wolf->angle;
-	rx = cos(rad(alpha));
-	ry = sin(rad(alpha));
+	y = (int)(wolf->posy) + (alpha > R180);
+	x = wolf->posx + (wolf->posy - y) / tan(alpha);
+	if (x < 0 || y < 0 || x > wolf->line || y * wolf->line > wolf->size)
+		return (-1);
+	i = (y + (alpha > R180)) * wolf->line + floor(x);
+	while (x && y && x != wolf->line &&
+	y * wolf->line != wolf->size && wolf->map[i] != WALL)
+	{
+		y += alpha > R180 ? 1 : -1;
+		x += 1 / tan(alpha);
+		if (x < 0 || y < 0 || x > wolf->line || y * wolf->line > wolf->size)
+			return (-1);
+		i = (y + (alpha > R180)) * wolf->line + floor(x);
+	}
+	return (fabs(wolf->posx - x) / cos(alpha));
 }
 
-void			render(t_wolf *wolf)
+static double		dist_x(t_wolf *wolf, double alpha)
+{
+	double	x;
+	double	y;
+	int		i;
+
+	x = (int)(wolf->posx) + (alpha < R90 || alpha > R270);
+	y = wolf->posy + (wolf->posx - x) * tan(alpha);
+	if (x < 0 || y < 0 || x > wolf->line || y * wolf->line > wolf->size)
+		return (-1);
+	i = y * wolf->line + floor(x) + (alpha < R90 || alpha > R270);
+	while (x && y && x != wolf->line &&
+	y * wolf->line != wolf->size && wolf->map[i] != WALL)
+	{
+		y += tan(alpha);
+		x += alpha < R90 || alpha > R270 ? 1 : -1;
+		if (x < 0 || y < 0 || x > wolf->line || y * wolf->line > wolf->size)
+			return (-1);
+		i = y * wolf->line + floor(x) + (alpha < R90 || alpha > R270);
+	}
+	return (fabs(wolf->posx - x) / cos(alpha));
+}
+
+static void			walls(int x, t_wolf *wolf)
+{
+	double	alpha;
+	double	dst[2];
+	int		height;
+	int		y;
+
+	alpha = rad(FOV - x / WINX * FOV + wolf->angle);
+	dst[0] = dist_x(wolf, alpha);
+	dst[1] = dist_y(wolf, alpha);
+	dst[0] == -1 || (dst[1] != -1 && dst[0] > dst[1]) ? dst[1] = dst[0] : 0;
+	height = 1 / (dst[0] * (cos(rad(alpha) - rad(FOV / 2)))) * wolf->dste;
+	height < 0 ? height = 0 : 0;
+	y = 0;
+	while (y < WINY)
+	{
+		if (y < WINY / 2 - height / 2)
+			mlx_pixel_put(wolf->mlx, wolf->win, x, y, 0x0000a0);
+		else if (y < WINY / 2 + height / 2)
+			mlx_pixel_put(wolf->mlx, wolf->win, x, y, 0xa0a0a0);
+		else
+			mlx_pixel_put(wolf->mlx, wolf->win, x, y, 0xffffff);
+		y++;
+	}
+}
+
+void				render(t_wolf *wolf)
 {
 	int		x;
 
--- srcs/run.c
+++ srcs/run.c
@@ -6,7 +6,7 @@
 /*   By: tberthie <tberthie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/16 13:09:57 by tberthie          #+#    #+#             */
-/*   Updated: 2017/01/19 17:15:10 by tberthie         ###   ########.fr       */
+/*   Updated: 2017/01/20 15:58:48 by tberthie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 #include <unistd.h>
 #include <mlx.h>
 
-static int	key(int code, t_wolf *wolf)
+int			key(int code, t_wolf *wolf)
 {
-	code = 0;
-	wolf = 0;
+	if (code == 0)
+		wolf->angle -= 5;
+	if (code == 2)
+		wolf->angle += 5;
+	render(wolf);
 	return (1);
 }
 
 void		run(t_wolf *wolf)
 {
+	wolf->mlx = 0;
+	wolf->win = 0;
 	if ((wolf->mlx = mlx_init()) &&
 	(wolf->win = mlx_new_window(wolf->mlx, WINX, WINY, "Wolf3D")))
 	{
@@ -31,6 +36,6 @@ void		run(t_wolf *wolf)
 		mlx_key_hook(wolf->win, key, wolf);
 		mlx_loop(wolf->mlx);
 	}
-	(!wolf->mlx) ? write(2, "wolf3d: mlx failed to init\n", 27) :
+	!wolf->mlx ? write(2, "wolf3d: mlx failed to init\n", 27) :
 	write(2, "wolf3d: failed to create window\n", 32);
 }
